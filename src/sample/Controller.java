e3b716ddd23ca5b1afc43309d4c7154cdeec1567
RedDragon1983 <maximov_n@inbox.ru>
2021-05-11 13:48:44 +0300

    Получение списка пользователей

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,12 @@ package sample;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import java.io.DataInputStream;
@@ -21,6 +25,13 @@ public class Controller {
     TextArea textArea;
     @FXML
     TextField textField;
+    @FXML
+    TextArea textAreaUserList;
+
+    Stage stage;
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
     @FXML
     private void onSubmit(){
@@ -34,22 +45,37 @@ public class Controller {
             exception.printStackTrace();
         }
     }
+
     @FXML
-    private void connect(){
+    private void buttonOn() throws Exception{
+
+        stage.setTitle("Клиент сетевого чата");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setScene(new Scene(root, 500, 300));
+
         try {
-            socket = new Socket("45.80.70.161",8188);
+            socket = new Socket("192.168.1.5",8188);
             DataInputStream in =new DataInputStream(socket.getInputStream());
-            out=new DataOutputStream(socket.getOutputStream());
+            out=new DataOutputStream(socket.getOutputStream()); // Инициализация out
             String response = in.readUTF(); // Ждём сообщение от сервера
-            textArea.appendText(response+"\n"); // Добро пожаловать на сервер
+            textArea.appendText(response+"\n"); // Введите имя
             thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     while(true){
                         try {
                             String response = in.readUTF(); // ждём сообщение от сервера
-                            textArea.appendText(response+"\n");
-                        } catch (IOException exception) {
+                            if(response.indexOf("**userlist**")==0){
+                                textAreaUserList.clear();
+                                String[] userList = response.split("//");
+                                for (String userName:userList) {
+                                    textAreaUserList.appendText(userName+"\n");
+                                }
+                            }else{
+                                textArea.appendText(response+"\n");
+                            }
+                        } catch (Exception exception) {
                             exception.printStackTrace();
                         }
                     }
@@ -59,6 +85,8 @@ public class Controller {
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+
+        //Platform.
     }
 
 }
